01034a609197da1ded075a3b7ecc91bdec302034
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2023-11-17 17:06:15 +0000

    add verbose

--- models/transformer.py
+++ models/transformer.py
@@ -308,6 +308,8 @@ class Transformer(nn.Module):
             
             s = time.time()
             for i, batch in enumerate(self.train_iter): 
+                if i==0:
+                    print(f"Shape: src {batch.src.shape}, tgt {batch.tgt.shape}")
                 loss = self.train_step(optimizer, batch, criterion)
                 total_loss += loss
                 
