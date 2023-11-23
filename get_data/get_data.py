1abe32cbdea151de2ed2e99b0ef06e6c898a6d60
Trần Bá Hoàng <87892619+hoangbros03@users.noreply.github.com>
2023-11-23 04:28:28 +0000

    add checking length

--- get_data/get_data.py
+++ get_data/get_data.py
@@ -1,127 +1,136 @@
+import argparse
 import requests
+
 from bs4 import BeautifulSoup
-
-url = 'https://truyenfull.vn/'
-response = requests.get(url)
-
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-list_stories = soup.find_all('div', class_="col-xs-9 col-sm-6 col-md-5 col-title")
-
-# print(list_stories)
-
-a_tags = []
-for story in list_stories:
-    a_tags.append(story.find('a'))
-
-    hrefs = []
-for a in a_tags:
-    hrefs.append(a.get('href'))
-
-# print(len(hrefs))
-# for link in hrefs:
-#     print(link)
-
-for i in range(len(hrefs)):
-    hrefs[i] += 'chuong-1'
-
-# print(len(hrefs))
-# for link in hrefs:
-#     print(link)
-
 import nltk
 import time
 
-nltk.download('punkt')
-results = []
-for link in hrefs:
-    print(link)
-    tam_response = requests.get(link)
-    if tam_response.status_code == 200:
-        tam_soup = BeautifulSoup(tam_response.content, 'html.parser')
-        paragraph = str(tam_soup.find('div', id = 'chapter-c').get_text)
+def process(url, output_file):
+    response = requests.get(url)
 
-        x = paragraph.find('</div>') + 6
-        y = len(paragraph) - 1
-        paragraph = paragraph[x:y]
-        paragraph = paragraph.replace('<br/><br/>', ' . ')
-        paragraph = paragraph.replace('</p><p><br/></p><p>', ' . ')
-        paragraph = paragraph.replace('<br/>', ' . ')
-        paragraph = paragraph.replace('<p><p><br/>', ' . ')
-        paragraph = paragraph.replace('</p></p></div>', ' . ')
-        paragraph = paragraph.replace('----------oOo----------', ' . ')
-        paragraph = paragraph.replace('</div>', ' . ')
-        paragraph = paragraph.replace('<p><p>', ' . ')
-        paragraph = paragraph.replace('... .', '...')
-        paragraph = paragraph.replace('...  .', '...')
-        paragraph = paragraph.replace('<p>', ' . ')
-        paragraph = paragraph.replace('</p>', ' . ')
-        sentences = nltk.sent_tokenize(paragraph)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-        for sentence in sentences:
-            if (sentence == '*** .'):
-                break
-            if (sentence != '.' 
-                and not sentence.startswith('.') 
-                and not sentence.startswith('…') 
-                and not sentence.startswith('*') 
-                and not sentence.startswith(' ')
-                and not sentence.startswith('!')
-                and not sentence.startswith('?')):
-                results.append(sentence)
-        next_button = tam_soup.find('a', id="next_chap")
-        next_link = next_button.get('href')
-        dem = 0
-        while (True): 
-            print(dem + 1)
-            if (next_button.get('title') != 'Không hoặc chưa có chương tiếp theo'):
-                next_link = next_button.get('href')
-                tam_response = requests.get(next_link)
-                if tam_response.status_code == 200:
-                    tam_soup = BeautifulSoup(tam_response.content, 'html.parser')
-                    paragraph = str(tam_soup.find('div', id = 'chapter-c').get_text)
+    list_stories = soup.find_all('div', class_="col-xs-9 col-sm-6 col-md-5 col-title")
 
-                    x = paragraph.find('</div>') + 6
-                    y = len(paragraph) - 1
-                    paragraph = paragraph[x:y]
-                    paragraph = paragraph.replace('<br/><br/>', ' . ')
-                    paragraph = paragraph.replace('</p><p><br/></p><p>', ' . ')
-                    paragraph = paragraph.replace('<br/>', ' . ')
-                    paragraph = paragraph.replace('<p><p><br/>', ' . ')
-                    paragraph = paragraph.replace('</p></p></div>', ' . ')
-                    paragraph = paragraph.replace('----------oOo----------', ' . ')
-                    paragraph = paragraph.replace('</div>', ' . ')
-                    paragraph = paragraph.replace('<p><p>', ' . ')
-                    paragraph = paragraph.replace('... .', '...')
-                    paragraph = paragraph.replace('...  .', '...')
-                    paragraph = paragraph.replace('<p>', ' . ')
-                    paragraph = paragraph.replace('</p>', ' . ')
-                    sentences = nltk.sent_tokenize(paragraph)
+    # print(list_stories)
 
-                    for sentence in sentences:
-                        if (sentence == '*** .'):
-                            break
-                        if (sentence != '.' 
-                            and not sentence.startswith('.') 
-                            and not sentence.startswith('…') 
-                            and not sentence.startswith('*') 
-                            and not sentence.startswith(' ')
-                            and not sentence.startswith('!')
-                            and not sentence.startswith('?')):
-                            results.append(sentence)
+    a_tags = []
+    for story in list_stories:
+        a_tags.append(story.find('a'))
 
-                    next_button = tam_soup.find('a', id="next_chap")
-                    dem += 1
+        hrefs = []
+    for a in a_tags:
+        hrefs.append(a.get('href'))
+
+    # print(len(hrefs))
+    # for link in hrefs:
+    #     print(link)
+
+    for i in range(len(hrefs)):
+        hrefs[i] += 'chuong-1'
+
+    # print(len(hrefs))
+    # for link in hrefs:
+    #     print(link)
+
+    nltk.download('punkt')
+    results = []
+    for link in hrefs:
+        print(link)
+        tam_response = requests.get(link)
+        if tam_response.status_code == 200:
+            tam_soup = BeautifulSoup(tam_response.content, 'html.parser')
+            paragraph = str(tam_soup.find('div', id = 'chapter-c').get_text)
+
+            x = paragraph.find('</div>') + 6
+            y = len(paragraph) - 1
+            paragraph = paragraph[x:y]
+            paragraph = paragraph.replace('<br/><br/>', ' . ')
+            paragraph = paragraph.replace('</p><p><br/></p><p>', ' . ')
+            paragraph = paragraph.replace('<br/>', ' . ')
+            paragraph = paragraph.replace('<p><p><br/>', ' . ')
+            paragraph = paragraph.replace('</p></p></div>', ' . ')
+            paragraph = paragraph.replace('----------oOo----------', ' . ')
+            paragraph = paragraph.replace('</div>', ' . ')
+            paragraph = paragraph.replace('<p><p>', ' . ')
+            paragraph = paragraph.replace('... .', '...')
+            paragraph = paragraph.replace('...  .', '...')
+            paragraph = paragraph.replace('<p>', ' . ')
+            paragraph = paragraph.replace('</p>', ' . ')
+            sentences = nltk.sent_tokenize(paragraph)
+
+            for sentence in sentences:
+                if (sentence == '*** .'):
+                    break
+                if (sentence != '.' 
+                    and not sentence.startswith('.') 
+                    and not sentence.startswith('…') 
+                    and not sentence.startswith('*') 
+                    and not sentence.startswith(' ')
+                    and not sentence.startswith('!')
+                    and not sentence.startswith('?')):
+                    results.append(sentence)
+            next_button = tam_soup.find('a', id="next_chap")
+            next_link = next_button.get('href')
+            dem = 0
+            while (True): 
+                print(dem + 1)
+                if (next_button.get('title') != 'Không hoặc chưa có chương tiếp theo'):
+                    next_link = next_button.get('href')
+                    tam_response = requests.get(next_link)
+                    if tam_response.status_code == 200:
+                        tam_soup = BeautifulSoup(tam_response.content, 'html.parser')
+                        paragraph = str(tam_soup.find('div', id = 'chapter-c').get_text)
+
+                        x = paragraph.find('</div>') + 6
+                        y = len(paragraph) - 1
+                        paragraph = paragraph[x:y]
+                        paragraph = paragraph.replace('<br/><br/>', ' . ')
+                        paragraph = paragraph.replace('</p><p><br/></p><p>', ' . ')
+                        paragraph = paragraph.replace('<br/>', ' . ')
+                        paragraph = paragraph.replace('<p><p><br/>', ' . ')
+                        paragraph = paragraph.replace('</p></p></div>', ' . ')
+                        paragraph = paragraph.replace('----------oOo----------', ' . ')
+                        paragraph = paragraph.replace('</div>', ' . ')
+                        paragraph = paragraph.replace('<p><p>', ' . ')
+                        paragraph = paragraph.replace('... .', '...')
+                        paragraph = paragraph.replace('...  .', '...')
+                        paragraph = paragraph.replace('<p>', ' . ')
+                        paragraph = paragraph.replace('</p>', ' . ')
+                        sentences = nltk.sent_tokenize(paragraph)
+
+                        for sentence in sentences:
+                            if (sentence == '*** .'):
+                                break
+                            if (sentence != '.' 
+                                and not sentence.startswith('.') 
+                                and not sentence.startswith('…') 
+                                and not sentence.startswith('*') 
+                                and not sentence.startswith(' ')
+                                and not sentence.startswith('!')
+                                and not sentence.startswith('?')):
+                                results.append(sentence)
+
+                        next_button = tam_soup.find('a', id="next_chap")
+                        dem += 1
+                    else:
+                        time.sleep(60)
                 else:
-                    time.sleep(60)
-            else:
-                break
-        break
+                    break
+            break
 
-# with open('link_to_file', 'w', encoding='utf-8') as file:
-#     for i in range (0, len(results)):
-#         if (i == 0):
-#             file.write(f'{results[i]}')
-#         else:
-#             file.write(f'\n{results[i]}')
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for i in range (0, len(results)):
+            if (i == 0):
+                file.write(f'{results[i]}')
+            else:
+                file.write(f'\n{results[i]}')
+
+if __name__ == "__main__":
+    parser= argparse.ArgumentParser()
+    parser.add_argument("-u", "--url", help="url", type=str, required=True)
+    parser.add_argument("-o","--output_file", help="output file dir", type=str, required= True)
+    args = parser.parse_args()
+    process(args.url, args.output_file)
+    
--- get_data/pre_process.py
+++ get_data/pre_process.py
@@ -4,23 +4,38 @@ Preprocess the data from .lo and .vi file
 import re
 import argparse
 
+# Regex of both .lo and .vi file
 reg = r'\(?[A-Za-z0-9\u0e80-\u0effaAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ]+\)?[.!?,:]?'
-def process(input_file, output_file):
+def process(input_src, input_tgt, output_source, output_target, max_len):
     result = []
     process_code = re.compile(reg, re.UNICODE)
-    with open(input_file, "r") as f:
-        data = f.readlines()
-    print(f"Total sentences: {len(data)}")
-    with open(output_file,"a") as f:
-        for i, d in enumerate(data):
-            f.write(" ".join(process_code.findall(d)))
-            if i < len(data)-1:
-                f.write("\n")
+    with open(input_src, "r") as file1, open(input_tgt, "r") as file2:
+        data1 = file1.readlines()
+        data2 = file2.readlines()
+    print(f"Total sentences: {len(data1)}")
+    with open(output_source,"w") as file1, open(output_target,"w") as file2:
+
+        for idx in range(len(data1)):
+            """
+            Trying to detect if more than max_len word -> discard
+            """
+            if max(len(data1[idx].split(" ")), len(data2[idx].split(" "))) > max_len:
+                continue
+            else:
+                file1.write(" ".join(process_code.findall(data1[idx])))
+                if idx < len(data1)-1:
+                    file1.write("\n")
+                file2.write(" ".join(process_code.findall(data2[idx])))
+                if idx < len(data2)-1:
+                    file2.write("\n")
     print("Completed!")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--input",help="input file", type=str, required=True)
-    parser.add_argument("-o","--output",help="output file", type=str, required=True)
+    parser.add_argument("-is","--input_source",help="input source file (.lo file)", type=str, required=True)
+    parser.add_argument("-it","--input_target",help="input target file (.vi file)", type=str, required=True)
+    parser.add_argument("-os","--output_source",help="output source file", type=str, required=True)
+    parser.add_argument("-ot","--output_target",help="output target file", type=str, required=True)
+    parser.add_argument("--max_len", help="max len of sentence", type=int, default=180)
     args = parser.parse_args()
-    process(args.input, args.output)
+    process(args.input_source, args.input_target, args.output_source, args.output_target, args.max_len)
